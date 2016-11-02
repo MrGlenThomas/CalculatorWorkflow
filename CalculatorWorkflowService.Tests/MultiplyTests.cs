9387a554e8e404b35b2b70e84fb0837e3848bc76
Glen Thomas <Glen Thomas>
2016-11-02 22:37:50 +0000

    Initial commit

---
+++ CalculatorWorkflowService.Tests/MultiplyTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorWorkflowService.Tests
+{
+    [TestClass]
+    public class MultiplyTests
+    {
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            var num1 = 2;
+            var num2 = 5;
+            var expectedResult = 10;
+            int actualResult;
+
+            using (var multiplyClient = new MultiplyWorkflowService.ServiceClient())
+            {
+                actualResult = multiplyClient.Multiply(num1, num2);
+            }
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
