8b4e32e501f61d3ef5c3ec06c5fca9ad669ff8ac
Glen Thomas <glen.thomas@outlook.com>
2016-11-03 17:49:51 +0000

    Updated tutorial source code with Part 4

---
+++ CalculatorWorkflowService.Web/CalculatorService.cs
@@ -0,0 +1,11 @@
+﻿namespace CalculatorWorkflowService.Web
+{
+    using System.ServiceModel;
+
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        int Multiply(int value1, int value2);
+    }
+}
---
+++ CalculatorWorkflowService.Web/CalculatorService.svc.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculatorWorkflowService.Web
+{
+    public class CalculatorService : ICalculatorService
+    {
+        public int Multiply(int value1, int value2)
+        {
+            return value1 * value2;
+        }
+    }
+}
